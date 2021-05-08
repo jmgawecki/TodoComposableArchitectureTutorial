b41c1835d6414b81236362d4ee991a925473f13b
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-05-08 21:52:07 +0100

    Initial Commit

---
+++ ToDoComposableArchitecture/ToDoComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoComposableArchitectureApp.swift
+//  ToDoComposableArchitecture
+//
+//  Created by Jakub Gawecki on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
